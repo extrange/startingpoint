5c316c3c6bdfaf66920eaba1bed83dd95bfe5923
extrange <29305375+extrange@users.noreply.github.com>
2023-10-27 12:51:05 +0800

    add startingpoint.yaml for use-sigstore-attachments support

--- config/scripts/signing.sh
+++ config/scripts/signing.sh
@@ -9,6 +9,7 @@ PKI_DIR=/usr/etc/pki/containers/
 mkdir -p $PKI_DIR
 cp /usr/share/ublue-os/cosign.pub "$PKI_DIR$IMAGE_NAME".pub
 
+# Modifying this file updates that on the current OS', allowing for signed updates (the default is insecureAcceptAnything, which does not perform verification).
 POLICY_JSON=/etc/containers/policy.json
 
 yq -i -o=j '.transports.docker |=
@@ -27,5 +28,5 @@ yq -i -o=j '.transports.docker |=
 IMAGE_REF="ostree-image-signed:docker://$IMAGE_REGISTRY/$IMAGE_NAME"
 printf '{\n"image-ref": "'"$IMAGE_REF"'",\n"image-tag": "latest"\n}' > /usr/share/ublue-os/image-info.json
 
-# cp /usr/etc/containers/registries.d/ublue-os.yaml /usr/etc/containers/registries.d/"$IMAGE_NAME".yaml
-# sed -i "s ghcr.io/ublue-os $IMAGE_REGISTRY g" /usr/etc/containers/registries.d/"$IMAGE_NAME".yaml
+# Enable use-sigstore-attachments on image
+cp /usr/etc/containers/registries.d/startingpoint.yaml /usr/etc/containers/registries.d/"$IMAGE_NAME".yaml
