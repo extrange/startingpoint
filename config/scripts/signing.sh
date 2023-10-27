f021fcb35cc1f139d1efa9cc11e89107b5214338
extrange <29305375+extrange@users.noreply.github.com>
2023-10-27 13:30:34 +0800

    copy policy.json to /usr/etc/containers/policy.json

--- config/scripts/signing.sh
+++ config/scripts/signing.sh
@@ -9,9 +9,11 @@ PKI_DIR=/usr/etc/pki/containers/
 mkdir -p $PKI_DIR
 cp /usr/share/ublue-os/cosign.pub "$PKI_DIR$IMAGE_NAME".pub
 
-# Modifying this file updates that on the current OS', allowing for signed updates (the default is insecureAcceptAnything, which does not perform verification).
+# For an explanation of policy.json, see
+# https://github.com/containers/image/blob/main/docs/containers-policy.json.5.md
 POLICY_JSON=/etc/containers/policy.json
 
+# This allows for signed updates (the default is insecureAcceptAnything, which does not perform verification).
 yq -i -o=j '.transports.docker |=
     {"'"$IMAGE_REGISTRY"'/'"$IMAGE_NAME"'": [
             {
@@ -24,6 +26,9 @@ yq -i -o=j '.transports.docker |=
         ]
     }
 + .' "$POLICY_JSON"
+
+# rpm-ostree checks the upstream image's /usr/etc/containers/policy.json
+cp $POLICY_JSON /usr/etc/containers/policy.json
 
 IMAGE_REF="ostree-image-signed:docker://$IMAGE_REGISTRY/$IMAGE_NAME"
 printf '{\n"image-ref": "'"$IMAGE_REF"'",\n"image-tag": "latest"\n}' > /usr/share/ublue-os/image-info.json
