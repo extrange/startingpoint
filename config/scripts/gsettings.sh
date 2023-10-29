270f803cc6275cc588e60e6acd8b993e6ab0095e
extrange <29305375+extrange@users.noreply.github.com>
2023-10-29 14:59:02 +0800

    add gsettings

--- config/scripts/gsettings.sh
+++ config/scripts/gsettings.sh
@@ -2,21 +2,12 @@
 
 set -oue pipefail
 
-array=( https://extensions.gnome.org/extension/1319/gsconnect/
-https://extensions.gnome.org/extension/779/clipboard-indicator/
-https://extensions.gnome.org/extension/4362/fullscreen-avoider/
-https://extensions.gnome.org/extension/5112/tailscale-status/ 
-https://extensions.gnome.org/extension/1460/vitals/ )
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
 
-for i in "${array[@]}"
-do
-    EXTENSION_ID=$(curl -s $i | grep -oP 'data-uuid="\K[^"]+')
-    VERSION_TAG=$(curl -Lfs "https://extensions.gnome.org/extension-query/?search=$EXTENSION_ID" | jq '.extensions[0] | .shell_version_map | map(.pk) | max')
-    wget -O "${EXTENSION_ID}.zip" "https://extensions.gnome.org/download-extension/${EXTENSION_ID}.shell-extension.zip?version_tag=$VERSION_TAG"
-    gnome-extensions install --force "${EXTENSION_ID}.zip"
-    if ! gnome-extensions list | grep --quiet "${EXTENSION_ID}"; then
-        busctl --user call org.gnome.Shell.Extensions /org/gnome/Shell/Extensions org.gnome.Shell.Extensions InstallRemoteExtension s "${EXTENSION_ID}"
-    fi
-    gnome-extensions enable "${EXTENSION_ID}"
-    rm "${EXTENSION_ID}.zip"
-done
+# Setup Alt-Tab to switch between windows instead of applications
+dconf write /org/gnome/desktop/wm/keybindings/switch-applications "@as []"
+dconf write /org/gnome/desktop/wm/keybindings/switch-windows "['<alt>Tab']"
+
+# Map capslock to backspace
+dconf write /org/gnome/desktop/input-sources/xkb-options "['caps:backspace']"
